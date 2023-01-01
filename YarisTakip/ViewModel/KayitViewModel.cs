456417a44455336cdaa1d9100a8d247ca3fd33ce
halilkeklik <halil@halilkeklik.com>
2023-01-01 17:01:20 +0300

    Viewler düzenlendi

--- YarisTakip/ViewModel/KayitViewModel.cs
+++ YarisTakip/ViewModel/KayitViewModel.cs
@@ -7,6 +7,7 @@ namespace YarisTakip.ViewModel
         [Display(Name = "Email Adresi")]
         [Required(ErrorMessage = "Email adresi gereklidir")]
         public string EmailAdres { get; set; }
+        [Display(Name = "Şifre")]
         [Required]
         [DataType(DataType.Password)]
         public string Sifre { get; set; }
